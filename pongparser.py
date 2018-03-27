631d78da92991c54e23e93b186c98ec8189d16a0
Oliver Grant <ogrant@ip-10-10-11-226.ec2.internal>
2018-03-27 08:20:30 -0400

    Added ranks

--- pongparser.py
+++ pongparser.py
@@ -31,34 +31,58 @@ def reverse_score(score):
 def get_pool_scores(offset):
 	results = []
 	for i in range(1+offset, 8 + offset):
-	  	for j in  range(1,8):
-	  		if i-offset == 1:
-	  			results.append([0,0,0,0,0,0])
-	  		elif scores[i][j] != '' and scores[i][j] != 'X':
-	  			res = parse_score(scores[i][j])
-	  			rev_res = reverse_score(res)
-	  			results[i-offset-2] = map(add, results[i-offset-2], res)
-	  			results[j-1] = map(add, results[j-1], rev_res)
-  	print(results)
-  	return(results)
+		for j in  range(1,8):
+			if i-offset == 1:
+				results.append([0,0,0,0,0,0])
+			elif scores[i][j] != '' and scores[i][j] != 'X':
+				res = parse_score(scores[i][j])
+				rev_res = reverse_score(res)
+				results[i-offset-2] = map(add, results[i-offset-2], res)
+				results[j-1] = map(add, results[j-1], rev_res)
+	print(results)
+	return(results)
 
 import csv
 with open('pong.csv', 'rb') as f:
-  reader = csv.reader(f)
-  scores = list(reader)
-  # Pool A
-  poola = get_pool_scores(0)
-  poolb = get_pool_scores(11)
-  poolc = get_pool_scores(22)
-  poold = get_pool_scores(33)
+	reader = csv.reader(f)
+	scores = list(reader)
+	# Pool A
+	poola = get_pool_scores(0)
+	poolb = get_pool_scores(11)
+	poolc = get_pool_scores(22)
+	poold = get_pool_scores(33)
 
-  for pool in [poola, poolb, poolc, poold]:
-	for row in pool:
-	  	for col in row:
-	  		print str(col) + ',',
-	 	print '\n',
-	print('\n\n\n')
+	scores = []
+	pool_scores = []
+	for pool in [poola, poolb, poolc, poold]:
+		pool_score = []
+		for player in pool:
+			score = 0.0
+			if (player[0] + player[1] != 0):
+				score += float(player[0]) / (float(player[0]) + float(player[1]))
+				score += (float(player[2]) - float(player[3]))/ 1000
+				score += (float(player[4]) - float(player[5])) / 1000000
+			else:
+				score = -1
+			scores.append(score)
+			pool_score.append(score)
+		pool_scores.append(pool_score)
 
-  		#print(scores[i][j])
+	sorted_scores = sorted(scores, reverse = True)
+	score_rank = [sorted_scores.index(v) + 1 for v in scores]
+	score_rank_pools = []
+	for i in range(4):
+		sorted_scores = sorted(pool_scores[i], reverse = True)
+		score_rank_pool = [sorted_scores.index(v) + 1 for v in pool_scores[i]]
+		score_rank_pools += score_rank_pool
 
-  #print(results)
+	p_index = 0
+	for pool in [poola, poolb, poolc, poold]:
+		for row in pool:
+			row.append(score_rank_pools[p_index])
+			row.append(score_rank[p_index])
+	  		for col in row:
+	  			print str(col) + ',',
+	 		print '\n',
+	 		p_index += 1
+		print('\n\n\n')
